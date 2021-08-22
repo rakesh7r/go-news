47f72e14593a19543b0f1c577321758959edc4f8
rakesh2002 <rakeshgandla200@gmail.com>
2021-08-22 15:27:45 +0530

    third api

--- src/Card.js
+++ src/Card.js
@@ -2,13 +2,7 @@ import React from "react"
 import { makeStyles } from "@material-ui/core/styles"
 import Card from "@material-ui/core/Card"
 import CardMedia from "@material-ui/core/CardMedia"
-import CardContent from "@material-ui/core/CardContent"
-import CardActions from "@material-ui/core/CardActions"
-import IconButton from "@material-ui/core/IconButton"
-import Typography from "@material-ui/core/Typography"
 import { red } from "@material-ui/core/colors"
-import FavoriteIcon from "@material-ui/icons/Favorite"
-import ShareIcon from "@material-ui/icons/Share"
 const useStyles = makeStyles((theme) => ({
     root: {
         maxWidth: 345,
@@ -49,8 +43,8 @@ function CardComp(props) {
                 <Card className={classes.root} style={{ width: "100%" }}>
                     <CardMedia
                         className={classes.media}
-                        // image={data.urlToImage}
-                        image={data.image}
+                        image={data.urlToImage}
+                        // image={data.image}
                         title="image"
                     />
                     {/* <CardContent>
--- src/NewsContext.js
+++ src/NewsContext.js
@@ -1,23 +1,23 @@
-import React, { createContext, useContext, useEffect, useState } from "react"
+import React, { useEffect, useState } from "react"
 import axios from "axios"
 import CardComp from "./Card"
 import "./NewsContext.css"
-
-const userContext = createContext()
 
 function NewsContext(props) {
     // const url =
     //     "https://newsapi.org/v2/top-headlines?country=in&apiKey=a8d48326a27c46bb9838a655f18b2b54"
 
-    const url =
-        "http://api.mediastack.com/v1/news?access_key=57f3d9e30bb445b1d9092b05c2169eac&country=in&sources=en&limit=100"
+    // const url =
+    //     "http://api.mediastack.com/v1/news?access_key=57f3d9e30bb445b1d9092b05c2169eac&country=in&sources=en&limit=100"
+
+    const url = "https://saurav.tech/NewsAPI/everything/cnn.json"
     const [data, setData] = useState(null)
     useEffect(() => {
         axios
             .get(url)
             .then((response) => {
-                console.log(response.data.data)
-                setData(response.data.data)
+                console.log(response.data.articles)
+                setData(response.data.articles)
             })
             .catch((err) => console.log(err.message))
     }, [])
